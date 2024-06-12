ff4bdf8c1e74867d72c8d38f00b0abb4e8220a32
Bruno Laitano <bruno.grigoletti.laitano@gmail.com>
2024-06-11 22:18:22 -0300

    Project created

---
+++ subscriptionmanagement/src/main/java/br/pucrs/bruno/laitano/subscriptionmanagement/SubscriptionmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.bruno.laitano.subscriptionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionmanagementApplication.class, args);
+	}
+
+}
---
+++ subscriptionmanagement/src/test/java/br/pucrs/bruno/laitano/subscriptionmanagement/SubscriptionmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pucrs.bruno.laitano.subscriptionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
