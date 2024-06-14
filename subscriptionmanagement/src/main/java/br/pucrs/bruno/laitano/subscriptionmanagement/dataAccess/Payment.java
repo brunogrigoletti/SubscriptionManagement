470f68207ecce99429d0b4cbf9fab39a22137218
Nathalia <nathaliagceretta@gmail.com>
2024-06-14 20:08:44 -0300

    classes User and Payment created

---
+++ subscriptionmanagement/src/main/java/br/pucrs/bruno/laitano/subscriptionmanagement/dataAccess/Payment.java
@@ -0,0 +1,59 @@
+package br.pucrs.bruno.laitano.subscriptionmanagement.dataAccess;
+
+import java.sql.Date;
+
+public class Payment {
+    private Long code;
+    private Subscription subscription;
+    private Double paymentValue;
+    private Date paymentDate;
+    private String promotion;
+
+    public Payment(Long code, Subscription subscription, Double paymentValue, Date paymentDate, String promotion) {
+        this.code = code;
+        this.subscription = subscription;
+        this.paymentValue = paymentValue;
+        this.paymentDate = paymentDate;
+        this.promotion = promotion;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public Subscription getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(Subscription subscription) {
+        this.subscription = subscription;
+    }
+
+    public Double getPaymentValue() {
+        return paymentValue;
+    }
+
+    public void setPaymentValue(Double paymentValue) {
+        this.paymentValue = paymentValue;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
+
+    public void setPromotion(String promotion) {
+        this.promotion = promotion;
+    }
+}
